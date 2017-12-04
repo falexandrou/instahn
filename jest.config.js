5420ce356fc660275da75e8aa5a5343ebddcca15
Fotis Alexandrou <falexandrou@gmail.com>
2017-12-04 16:26:43 +0200

    add isomorphic fetch to fix the tests on circleci

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,5 @@
 const path = require('path');
-
-// Replace global fetch with mock otherwise Response and Request objects are not defined
-// https://github.com/jefflau/jest-fetch-mock/issues/13#issuecomment-299413329
-const { Response, Headers, Request } = require('whatwg-fetch');
-
-global.Response = Response;
-global.Headers = Headers;
-global.Request = Request;
+const isomorphicFetch = require('isomorphic-fetch');
 
 module.exports = {
   moduleDirectories: [
@@ -14,8 +7,8 @@ module.exports = {
     path.resolve(__dirname, 'node_modules'),
   ],
   globals: {
-    Response: Response,
-    Headers: Headers,
-    Request: Request,
+    Response: global.Response,
+    Headers: global.Headers,
+    Request: global.Request,
   }
 };
